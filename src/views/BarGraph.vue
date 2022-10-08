e17a1b8f8dbb45dd5998973d2e7c777f70beec8c
kookiedncream <kim76744@naver.com>
2022-10-09 00:21:47 +0900

    feat: 구부림정도 표로 추가

--- src/views/BarGraph.vue
+++ src/views/BarGraph.vue
@@ -8,6 +8,29 @@
       :data="getChartInfo()"
       :options="chartOptions"
     />
+    <div class="flex">
+      <table id="table">
+        <caption>
+          [구부림 정도]
+        </caption>
+        <thead>
+          <tr>
+            <th class="text-left">엄지</th>
+            <th class="text-left">검지</th>
+            <th class="text-left">중지</th>
+            <th class="text-left">약지</th>
+            <th class="text-left">새끼</th>
+          </tr>
+        </thead>
+        <tbody>
+          <td>{{ flexes.thumb }}</td>
+          <td>{{ flexes.indexFinger }}</td>
+          <td>{{ flexes.middleFinger }}</td>
+          <td>{{ flexes.ringFinger }}</td>
+          <td>{{ flexes.littleFinger }}</td>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -24,11 +47,9 @@ export default {
 
   data() {
     return {
-      sensor: {
-        flexes: [],
-        pressures: [],
-        criteria: [],
-      },
+      flexes: [],
+      pressures: [],
+      criteria: [],
 
       chartOptions: {
         hAxis: {
@@ -52,27 +73,11 @@ export default {
     getChartInfo() {
       return [
         ["손가락", "기준", "사용자"],
-        ["엄지", this.sensor.criteria.thumb, this.sensor.pressures.thumb],
-        [
-          "검지",
-          this.sensor.criteria.indexFinger,
-          this.sensor.pressures.indexFinger,
-        ],
-        [
-          "중지",
-          this.sensor.criteria.middleFinger,
-          this.sensor.pressures.middleFinger,
-        ],
-        [
-          "약지",
-          this.sensor.criteria.ringFinger,
-          this.sensor.pressures.ringFinger,
-        ],
-        [
-          "새끼",
-          this.sensor.criteria.littleFinger,
-          this.sensor.pressures.thumb,
-        ],
+        ["엄지", this.criteria.thumb, this.pressures.thumb],
+        ["검지", this.criteria.indexFinger, this.pressures.indexFinger],
+        ["중지", this.criteria.middleFinger, this.pressures.middleFinger],
+        ["약지", this.criteria.ringFinger, this.pressures.ringFinger],
+        ["새끼", this.criteria.littleFinger, this.pressures.thumb],
       ];
     },
     async getData() {
@@ -85,9 +90,9 @@ export default {
         })
         .then((res) => {
           console.log(res.data);
-          this.sensor.criteria = res.data.criteria;
-          this.sensor.flexes = res.data.flexes;
-          this.sensor.pressures = res.data.pressures;
+          this.criteria = res.data.criteria;
+          this.flexes = res.data.flexes;
+          this.pressures = res.data.pressures;
         })
         .catch((err) => console.log(err));
     },
@@ -98,5 +103,34 @@ export default {
 #chart {
   border: 5px;
   height: 700px;
+}
+
+#table {
+  border: 1px solid black;
+  border-collapse: collapse;
+  width: 100%;
+  max-width: 800px;
+}
+tr {
+  background: whitesmoke;
+}
+
+th,
+td {
+  border: 1px solid black;
+  padding: 20px;
+  padding-left: 40px;
+  padding-right: 40px;
+}
+
+.flex {
+  display: grid;
+  place-content: center;
+  margin-bottom: 50px;
+}
+
+caption {
+  font-size: x-large;
+  margin-bottom: 10px;
 }
 </style>

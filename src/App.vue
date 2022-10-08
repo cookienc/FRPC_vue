0f901b0bceaba68a0c8a8c7dcbbe1466e5626063
kookiedncream <kim76744@naver.com>
2022-10-09 02:20:39 +0900

    feat: 선 그래프 추가

--- src/App.vue
+++ src/App.vue
@@ -65,6 +65,21 @@ export default {
     test() {
       alert("클릭됨");
     },
+
+    getDate() {
+      var today = new Date();
+
+      var year = today.getFullYear();
+      var month = ("0" + (today.getMonth() + 1)).slice(-2);
+      var day = ("0" + today.getDate()).slice(-2);
+      var dateString = year + "-" + month + "-" + day;
+
+      var hours = ("0" + today.getHours()).slice(-2);
+      var minutes = ("0" + today.getMinutes()).slice(-2);
+      var seconds = ("0" + today.getSeconds()).slice(-2);
+      var timeString = dateString + " " + hours + ":" + minutes + ":" + seconds;
+      return timeString;
+    },
   },
 };
 </script>
--- src/views/LineGraph.vue
+++ src/views/LineGraph.vue
@@ -1,3 +1,72 @@
 <template>
-  <h1>LineGraph</h1>
+  <div id="app">
+    <GChart type="LineChart" :data="getPressures()" :options="chartOptions" />
+  </div>
 </template>
+
+<script>
+import axios from "axios";
+import { GChart } from "vue-google-charts";
+
+export default {
+  name: "App",
+  components: {
+    GChart,
+  },
+
+  data() {
+    return {
+      pressures: [],
+
+      chartOptions: {
+        chart: {
+          title: "Company Performance",
+          subtitle: "Sales, Expenses, and Profit: 2014-2017",
+        },
+      },
+    };
+  },
+
+  created() {
+    this.getData();
+  },
+
+  methods: {
+    getDate() {
+      return this.$route.query.date;
+    },
+
+    async getData() {
+      await axios
+        .get("http://localhost:8080/home/line-graph", {
+          params: {
+            date: this.getDate(),
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          for (var i = 0; i < res.data.length; i++) {
+            this.pressures.push(res.data[i]);
+          }
+        })
+        .catch((err) => console.log(err));
+    },
+
+    getPressures() {
+      var list = [["날짜", "엄지", "검지", "중지", "약지", "새끼"]];
+
+      for (var i in this.pressures) {
+        var tmp = [];
+        tmp.push(this.pressures[i].date.substr(0, 10));
+        tmp.push(this.pressures[i].thumb);
+        tmp.push(this.pressures[i].indexFinger);
+        tmp.push(this.pressures[i].middleFinger);
+        tmp.push(this.pressures[i].ringFinger);
+        tmp.push(this.pressures[i].littleFinger);
+        list.push(tmp);
+      }
+      return list;
+    },
+  },
+};
+</script>
